9a6ff6042a263990d6f0138fd2f30f13bc994e76
Denis Kolesnichenko <kolesnichenko-den@mail.ru>
2022-07-19 00:30:50 +0300

    finish: form state is done

--- src/components/form/Form1.jsx
+++ src/components/form/Form1.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { useState } from 'react';
 import { useForm } from "react-hook-form";
 import InputMask from 'react-input-mask';
+import okey from '../../icons/icon_okey.svg';
 //import { withRouter } from "react-router-dom";
 //import { useStateMachine } from "little-state-machine";
 //import updateAction from "./updateAction";
@@ -18,41 +20,55 @@ const Form = props => {
     } = useForm();
     //    const { actions } = useStateMachine({ updateAction });
 
-    const onSubmit = (data) => {
+    const [
+        isSuccess,
+        setSuccess,
+    ] = useState(false);
+
+    let onSubmit = (data) => {
         console.log(JSON.stringify(data));
-        //        actions.updateAction(data);
-        //props.history.push("./Result");
+        console.log(isSuccess);
         reset();
+        setSuccess(true);
     }
 
     return (
-        <form className="container__form" onSubmit={handleSubmit(onSubmit)}>
-            <div className="container__form-field">
-                <label className="container__form-label">Ваш номер телефона</label>
-                <InputMask placeholder="+7 (___) ___ - __ - __" type="tel" mask="+7 (999) 999-99-99" id="tel" name="tel"
-                    onChange={e => this.handleChange(e, "+7 (____) ___ - __ - __")}
-                    {...register("telephone",
-                        {
-                            required: "Поле обязательно к заполнению!",
-                            minLength: { value: 16, message: 'Номер введён неверно!' },
-                            //maxLength: { value: 17, message: 'Номер введён неверно!' },
-                        })} />
-                {errors?.telephone && <div style={{ height: 20 }}><p style={{ color: 'red' }}>{errors?.telephone?.message || 'Неправильно введён номер телефона!'}</p></div>}
-            </div>
-            <div className="container__form-field">
-                <label className="container__form-label">Введите e-mail, который связан с этим номером</label>
-                <input placeholder="Example@gmail.com" type="email" id="email" name="email"
-                    onChange={e => this.handleChange(e, 'phone')}
-                    {...register("email",
-                        {
-                            required: "Поле обязательно к заполнению!",
-                            minLength: { value: 5, message: 'Почта введена неверно!' },
-                        })} />
-                {errors?.email && <div style={{ height: 20 }}><p style={{ color: 'red' }}>{errors?.email?.message || 'Неправильно введён номер телефона!'}</p></div>}
-            </div>
-            <p className="form__paragraph">Нажимая на кнопку, я принимаю <a href="11">Политику конфиденциальности</a> и соглашаюсь получать сообщения.</p>
-            <button>Теперь всё верно</button>
-        </form>
+        <>
+            {!isSuccess && <form className="container__form" onSubmit={handleSubmit(onSubmit)}>
+                <div className="container__form-field">
+                    <label className="container__form-label">Ваш номер телефона</label>
+                    <InputMask placeholder="+7 (___) ___ - __ - __" type="tel" mask="+7 (999) 999-99-99" id="tel" name="tel"
+                        onChange={e => this.handleChange(e, "+7 (____) ___ - __ - __")}
+                        {...register("telephone",
+                            {
+                                required: "Поле обязательно к заполнению!",
+                                minLength: { value: 16, message: 'Номер введён неверно!' },
+                            })} />
+                    {errors?.telephone && <div style={{ height: 20 }}><p style={{ color: 'red' }}>{errors?.telephone?.message || 'Неправильно введён номер телефона!'}</p></div>}
+                </div>
+                <div className="container__form-field">
+                    <label className="container__form-label">Введите e-mail, который связан с этим номером</label>
+                    <input placeholder="Example@gmail.com" type="email" id="email" name="email"
+                        onChange={e => this.handleChange(e, 'phone')}
+                        {...register("email",
+                            {
+                                required: "Поле обязательно к заполнению!",
+                                minLength: { value: 5, message: 'Почта введена неверно!' },
+                            })} />
+                    {errors?.email && <div style={{ height: 20 }}><p style={{ color: 'red' }}>{errors?.email?.message || 'Неправильно введён номер телефона!'}</p></div>}
+                </div>
+                <p className="form__paragraph">Нажимая на кнопку, я принимаю <a href="11">Политику конфиденциальности</a> и соглашаюсь получать сообщения.</p>
+                <button>Теперь всё верно</button>
+            </form>
+            }
+            {
+                isSuccess && <form className="container__form2" onSubmit={handleSubmit(onSubmit)}>
+                    <img className="container__form2-image" src={okey} alt='okey' />
+                    <h5 className="container__form2-title" >Изменения сохранены</h5>
+                    <p className="container__form2-text">Вы успешно привязали номер телефона к вашему e-mail адресу</p>
+                    <a className="container__form2-link" href="11">Изменить данные</a>
+                </form>
+            }</>
     );
 }
 
